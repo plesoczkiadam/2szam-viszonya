04cdeb2e6278cb3a0cd98f385c705dc8e8d69409
Ádám Plesoczki <43034243+plesoczkiadam@users.noreply.github.com>
2022-12-11 09:13:30 +0100

    Add files via upload

---
+++ 2 szam viszonya/2 szam viszonya/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_szam_viszonya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double szam1, szam2;
+            Console.WriteLine("Kérek két számot!");
+            Console.Write("Elsö szám = ");
+            szam1 = Convert.ToDouble(Console.ReadLine());
+           Console.Write("Második szám = ");
+            szam2 = Convert.ToDouble(Console.ReadLine());
+             if (szam1 < szam2) 
+                {
+                Console.WriteLine("\nA {0} kissebb,mint {1}.",szam1,szam2);
+                 }
+                else
+                {
+                if (szam1 == szam2) 
+                    {
+                    Console.WriteLine("\nA {0} egyenlő {1}-gyel.",szam1, szam2);
+                    }
+                    else
+                    {
+                    Console.WriteLine("\nA {1} kissebb,mint {0}.",szam1, szam2);
+                    }
+                }
+            Console.ReadLine();
+        }
+    }
+}
